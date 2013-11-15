9f044e9705bf282cba388ed52ac19d51f96996a8
Russell Smith <russell.smith@ukd1.co.uk>
2013-11-15 15:03:43 -0800

    Instructions, better links

--- server.rb
+++ server.rb
@@ -42,7 +42,8 @@ get '/' do
 </head>
 <body>
 <h1>Generate redirect rules for Amazon S3</h1>
-<p>This is opensource <a href='https://github.com/rainforestapp/amazon-s3-redirects'>on github</a>. Example:
+<p>This is opensource <a href='https://github.com/rainforestapp/amazon-s3-redirects'>on github</a> by <a href='https://www.rainforestqa.com/'>Rainforest QA</a> from the <a href='https://gist.github.com/chitsaou/5319661'>original gist</a> by <a href='https://github.com/chitsaou'>chitsaou</a>.
+<p>Example:
 <pre>
 /home                        /
 /products/iphone/specs.html  /iphone/specs.html
@@ -52,9 +53,10 @@ get '/' do
 /products                    /
 </pre>
 
+<p>Paste your stuff in here:
 <form method='post'>
   <p><textarea name='text' cols=80 rows=8></textarea>
-  <p><input type='submit'>
+  <p><input type='submit' value='Give me the XML'>
 </form>
 "
 end
